2ec9c671ab0d69aaab008b242db59fc153b42377
irenaYo <irenatugushi997@gmail.com>
2026-05-30 12:26:04 +0300

    add models

---
+++ MusicTracker/Domain/Models/Artist.cs
@@ -0,0 +1,6 @@
+namespace Domain.Models;
+
+public class Artist
+{
+    public string Name { get; set; }
+}
---
+++ MusicTracker/Domain/Models/Track.cs
@@ -0,0 +1,8 @@
+namespace Domain.Models;
+
+public class Track
+{
+    public string Name { get; set; }
+    public int Duration { get; set; }
+    public Artist Artist { get; set; }
+}
