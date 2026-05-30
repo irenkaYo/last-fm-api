e6d623e62726f1645105d117731fbf931a955f6a
irenaYo <irenatugushi997@gmail.com>
2026-05-30 12:54:37 +0300

    add endpoint for getting top tracks

---
+++ MusicTracker/API/Controllers/TrackController.cs
@@ -0,0 +1,21 @@
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class TrackController : ControllerBase
+{
+    private readonly ITrackService _trackService;
+
+    public TrackController(ITrackService trackService)
+    {
+        _trackService = trackService;
+    }
+
+    [HttpGet("top")]
+    public async Task<IActionResult> GetTopTracks([FromQuery] string userName)
+    {
+        var tracks = await _trackService.GetTopTracks(userName);
+        return Ok(tracks);
+    }
+}
