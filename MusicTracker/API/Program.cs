49bd91eda9948e110adc5448ed7e7f0bd9af99c1
irenaYo <irenatugushi997@gmail.com>
2026-05-30 12:55:08 +0300

    register application services

---
+++ MusicTracker/API/Program.cs
@@ -0,0 +1,27 @@
+using Application.Interfaces;
+using Application.Services;
+using Infrastructure.ExternalServices.MusicApi;
+using Infrastructure.Settings;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.Configure<MusicApiSettings>(
+    builder.Configuration.GetSection("MusicApi"));
+
+builder.Services.AddScoped<ITrackService, TrackService>();
+builder.Services.AddHttpClient<IMusicApiClient, MusicApiClient>();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
