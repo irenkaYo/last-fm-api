eef567128b600c1ec8709ba57c5b2c1b0664e732
irenaYo <irenatugushi997@gmail.com>
2026-05-31 15:34:07 +0300

    update Track.cs

--- MusicTracker/Domain/Models/Track.cs
+++ MusicTracker/Domain/Models/Track.cs
@@ -2,7 +2,8 @@ namespace Domain.Models;
 
 public class Track
 {
+    public Guid Id { get; set; }
     public string Name { get; set; }
     public int Duration { get; set; }
-    public Artist Artist { get; set; }
+    public string ArtistName { get; set; }
 }
