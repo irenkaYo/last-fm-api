28d305ece7ef43d2c6d7c56e56f74cde042dcafc
irenaYo <irenatugushi997@gmail.com>
2026-05-31 21:55:33 +0300

    create migration for changing type of duration

---
+++ MusicTracker/Infrastructure/Migrations/20260531184635_ChangeDurationToTimeSpan.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeDurationToTimeSpan : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                                 ALTER TABLE "Tracks"
+                                 ALTER COLUMN "Duration"
+                                 TYPE interval
+                                 USING make_interval(secs => "Duration");
+                                 """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                                 ALTER TABLE "Tracks"
+                                 ALTER COLUMN "Duration"
+                                 TYPE integer
+                                 USING EXTRACT(EPOCH FROM "Duration")::integer;
+                                 """);
+        }
+    }
+}
