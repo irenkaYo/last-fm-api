16557bb264345c2d37ad624137856d5d50e80a45
irenaYo <irenatugushi997@gmail.com>
2026-05-31 19:41:49 +0300

    delete unnecessary dtos

--- MusicTracker/Application/DTOs/External/LastFm/Tracks/Top/TopTracksDto.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace Application.DTOs.External;
-
-public class TopTracksDto
-{
-    [JsonPropertyName("track")]
-    public List<TrackDto> Tracks { get; set; }
-}
--- MusicTracker/Application/DTOs/External/LastFm/Tracks/Top/TopTracksResponseDto.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace Application.DTOs.External;
-
-public class TopTracksResponseDto
-{
-    [JsonPropertyName("toptracks")]
-    public TopTracksDto TopTracks { get; set; }
-}
