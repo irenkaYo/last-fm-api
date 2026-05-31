6d0895559596e88a9aa173120d89f6879f0835fc
irenaYo <irenatugushi997@gmail.com>
2026-05-31 15:35:42 +0300

    add method for getting track info

--- MusicTracker/Infrastructure/ExternalServices/MusicApi/MusicApiClient.cs
+++ MusicTracker/Infrastructure/ExternalServices/MusicApi/MusicApiClient.cs
@@ -1,5 +1,6 @@
 using System.Net.Http.Json;
 using Application.DTOs.External;
+using Application.DTOs.External.Info;
 using Application.DTOs.External.RecentTracks;
 using Application.Interfaces;
 using Infrastructure.Settings;
@@ -21,5 +22,10 @@ public class MusicApiClient : IMusicApiClient
     public async Task<RecentTracksResponseDto?> GetUserRecentTracks(string userName)
     {
         return await _httpClient.GetFromJsonAsync<RecentTracksResponseDto>($"2.0/?method=user.getrecenttracks&user={userName}&api_key={_apiKey}&format=json");
+    }
+
+    public async Task<TrackInfoResponseDto?> GetTrackInfo(string trackName, string artistName)
+    {
+        return await _httpClient.GetFromJsonAsync<TrackInfoResponseDto>($"2.0/?method=track.getInfo&api_key={_apiKey}&artist={artistName}&track={trackName}&format=json");
     }
 }
