4db2839430e7c033be201897ca505be989db43a9
irenaYo <irenatugushi997@gmail.com>
2026-05-31 21:02:05 +0300

    update Program.cs

--- MusicTracker/API/Program.cs
+++ MusicTracker/API/Program.cs
@@ -1,9 +1,17 @@
 using Application.Interfaces;
+using Application.Interfaces.Repositories;
 using Application.Services;
 using Infrastructure.ExternalServices.MusicApi;
+using Infrastructure.Persistence;
+using Infrastructure.Persistence.Repositories;
 using Infrastructure.Settings;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MusicTrackerDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 
@@ -14,7 +22,9 @@ builder.Services.Configure<MusicApiSettings>(
     builder.Configuration.GetSection("MusicApi"));
 
 builder.Services.AddScoped<ITrackService, TrackService>();
-builder.Services.AddScoped<IArtistService, ArtistService>();
+builder.Services.AddScoped<ITrackRepository, TrackRepository>();
+builder.Services.AddScoped<ISyncService, SyncService>();
+builder.Services.AddScoped<IListeningHistoryRepository, ListeningHistoryRepository>();
 builder.Services.AddHttpClient<IMusicApiClient, MusicApiClient>(client =>
 {
     client.BaseAddress = new Uri("https://ws.audioscrobbler.com/");
