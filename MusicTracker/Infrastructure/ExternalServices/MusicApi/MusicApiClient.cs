79e04578fb478f2d048811da68f03e6805f184fb
irenaYo <irenatugushi997@gmail.com>
2026-06-02 20:03:22 +0300

    add custom exception handling for Last.fm API responses

--- MusicTracker/Infrastructure/ExternalServices/MusicApi/MusicApiClient.cs
+++ MusicTracker/Infrastructure/ExternalServices/MusicApi/MusicApiClient.cs
@@ -1,6 +1,8 @@
+using System.Net;
 using System.Net.Http.Json;
 using Application.DTOs.External.Info;
 using Application.DTOs.External.RecentTracks;
+using Application.Exceptions;
 using Application.Interfaces;
 using Infrastructure.Settings;
 using Microsoft.Extensions.Options;
@@ -20,14 +22,40 @@ public class MusicApiClient : IMusicApiClient
 
     public async Task<RecentTracksResponseDto?> GetUserRecentTracks(string userName)
     {
-        return await _httpClient.GetFromJsonAsync<RecentTracksResponseDto>($"2.0/?method=user.getrecenttracks&user={userName}&api_key={_apiKey}&format=json");
+        HttpResponseMessage response = await _httpClient.GetAsync(
+            $"2.0/?method=user.getrecenttracks&user={userName}&api_key={_apiKey}&format=json");
+
+        if (response.StatusCode == HttpStatusCode.NotFound)
+        {
+            throw new NotFoundException("User not found");
+        }
+        
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new ExternalServiceException("Last.fm service is unavailable");
+        }
+
+        return await response.Content.ReadFromJsonAsync<RecentTracksResponseDto>();    
     }
 
     public async Task<TrackInfoResponseDto?> GetTrackInfo(string trackName, string artistName)
     {
         var artist = Uri.EscapeDataString(artistName);
         var track = Uri.EscapeDataString(trackName);
+
+        HttpResponseMessage response = await _httpClient.GetAsync(
+            $"2.0/?method=track.getInfo&api_key={_apiKey}&artist={artist}&track={track}&format=json");
+
+        if (response.StatusCode == HttpStatusCode.NotFound)
+        {
+            throw new NotFoundException("User not found");
+        }
         
-        return await _httpClient.GetFromJsonAsync<TrackInfoResponseDto>($"2.0/?method=track.getInfo&api_key={_apiKey}&artist={artist}&track={track}&format=json");
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new ExternalServiceException("Last.fm service is unavailable");
+        }
+
+        return await response.Content.ReadFromJsonAsync<TrackInfoResponseDto>();
     }
 }
